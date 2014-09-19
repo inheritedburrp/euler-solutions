bf262c30d736559054e4a40a5ef7dcb3ebca7ac7
Anuj <anuj.want002@gmail.com>
2014-09-20 02:58:51 +0530

    Create prob5.py

---
+++ prob5.py
@@ -0,0 +1,28 @@
+import time
+ 
+def evenlyDivisible(n):
+    counter = 0
+    for i in range(1, 21):
+        if n % i == 0:
+            counter = counter + 1
+            if counter == 20:
+                return n
+        else:
+            return 1
+ 
+def main():
+    start_time = time.clock()
+    n = 2520
+    switch = 1
+    while switch == 1:
+        smallest = evenlyDivisible(n)
+        if smallest != 1:
+            print smallest
+            switch = 0
+        else:
+            n += 2520
+    run_time = time.clock() - start_time
+    print "Run time = ", run_time
+     
+if __name__ == '__main__':
+    main()
