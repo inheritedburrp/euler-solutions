1b7af86c0350356fead3d0bd35613813f2522b57
Anuj <anuj.want002@gmail.com>
2014-09-21 06:57:02 +0530

    Update prob2.py

--- prob2.py
+++ prob2.py
@@ -3,14 +3,12 @@ sum = 2
 def fibonacci(n):
 	global sum
 	lst = [1,2]
-	#print "1"
-	#print "2"
 	for i in xrange(0,n-2):
 		lst.append(int(lst[-1])+int(lst[-2]))
 		
 		if lst[-1] % 2 == 0:
 			sum += lst[-1]
-		if lst[-1] > 4000000:
+		elif lst[-1] > 4000000:
 			return
 		
 
